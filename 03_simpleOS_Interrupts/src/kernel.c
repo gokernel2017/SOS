d5d0f73f9c7c189cb5ca842ebaff2bc363aa7e93
gokernel2017 <31101188+gokernel2017@users.noreply.github.com>
2019-06-17 19:50:57 -0300

    Add files via upload

---
+++ 03_simpleOS_Interrupts/src/kernel.c
@@ -0,0 +1,143 @@
+//-------------------------------------------------------------------
+//
+// THIS FILE IS PART OF SIMPLE OPERATING SYSTEM:
+//
+//   The Kernel Core.
+//
+// MAIN FUNCTION:
+//
+//   void kernel_main ( struct multiboot_info * mbi );
+//
+// FILE:
+//   kernel.c
+//
+// START DATE: 16/06/2019 - 10:40
+//
+//-------------------------------------------------------------------
+//
+#include "simple.h"
+
+#define STR_CMD_SIZE    255
+#define SHELL_PROMPT    "\nshell > "
+
+static char
+    string_command [ STR_CMD_SIZE + 1 ]
+    ;
+
+unsigned long timer_ticks = 0;
+
+extern void print_time (void);
+
+void timer_handler (registers *reg) {
+
+    timer_ticks++;
+
+    if (timer_ticks % CLOCKS_PER_SEC == 0) {
+
+        //
+        // display ( EACH SECOND ) on top of screen:
+        //   0 ZERO
+        //   1 Primeiro
+        //   2 Segundo
+        //   ...
+        //   9 Nono
+        //
+        print_time ();
+    }
+}
+
+void init_timer (int hz) {
+    int divisor = PIT_HZ / hz;
+    outb(0x43, 0x36);
+    outb(0x40, divisor & 0xFF);
+    outb(0x40, divisor >> 8);
+    Intr_SetHandler (IRQ_TIMER, timer_handler);
+}
+
+int kernel_init (struct multiboot_info * mbi) {
+    video_clear ();
+
+    //-------------------------------------------
+    // INTERRUPTS:
+    //-------------------------------------------
+
+    Intr_Init (); // interrupts API:
+    init_timer (CLOCKS_PER_SEC);
+
+    //-------------------------------------------
+
+    puts ("Kernel Init ...\n");
+    return 1;
+}
+
+void kernel_finalize (void) {
+    puts ("Kernel Finalize ...\n");
+}
+
+int GetCommandLine (char *string, int len) {
+    static int pos = 0;
+    int k;
+
+    k = keyboard_getkey ();
+
+    if (k > 0) {
+        if (k == KEY_ENTER) {
+            string [pos] = 0;
+            pos = 0;
+            return KEY_ENTER;
+        } else if (k == KEY_BACKSPACE) {
+            if (pos > 0) {
+                pos--;
+                video_backspace ();
+            }
+        } else if (k == KEY_TAB || k == KEY_ALT) {
+
+            return k;
+
+        } else if (pos < len && (k >= KEY_SPACE && k <= 126)) { //  126 = ( ~ )
+            char buf [5] = { 0, 0, 0, 0 };
+            string [pos++] = k;
+            buf [0] = k;
+            buf [1] = 0;
+            puts (buf);
+        } else {
+            //return key;
+        }
+    }
+
+    return 0;
+
+}// GetCommandLine()
+
+void kernel_main_loop (void) {
+
+    puts ("\nKeyboard loop example ... NO INTERRUPTS\n");
+    puts ("To exit type: 'quit'\n");
+    puts (SHELL_PROMPT);
+
+    for (;;) {
+
+        keyboard_wait ();
+
+        switch (GetCommandLine(string_command, STR_CMD_SIZE)) {
+        case KEY_ENTER:
+            if (!strcmp(string_command, "quit")) {
+                puts ("\n");
+                return;
+            }
+            puts("\nCommand Not Found: '"); puts(string_command); puts("'");
+            puts (SHELL_PROMPT);
+            break;
+        }// switch (GetCommandLine(...))
+
+    }// for (;;)
+
+}// kernel_main_loop()
+
+void kernel_main (struct multiboot_info * mbi) {
+    if (!kernel_init(mbi))
+  return;
+    kernel_main_loop ();
+    kernel_finalize ();
+}
+
---
+++ 03_simpleOS_Interrupts/src/simple.h
@@ -0,0 +1,123 @@
+//-------------------------------------------------------------------
+//
+// THIS FILE IS PART OF SIMPLE OPERATING SYSTEM:
+//
+//   The Main Header:
+//
+// FILE:
+//   simple.h
+//
+// START DATE: 16/06/2019 - 10:40
+//
+//-------------------------------------------------------------------
+//
+#ifndef _SIMPLE_H_
+#define _SIMPLE_H_
+
+//-----------------------------------------------
+//------------------  INCLUDE  ------------------
+//-----------------------------------------------
+//
+#include "multiboot.h"
+
+//-----------------------------------------------
+//---------------  DEFINE / ENUM  ---------------
+//-----------------------------------------------
+//
+
+#define NULL                  ((void*)0)
+#define TXT_COLOR             7
+#define MAXLEN                1024
+
+#define PIT_HZ                1193182
+#define CLOCKS_PER_SEC        1000
+#define IRQ_TIMER             0
+#define IRQ_KEYBOARD          1
+
+// keys:
+#define KEY_BACKSPACE         8
+#define KEY_TAB               9
+#define KEY_ENTER             13
+#define KEY_ESC               27
+#define KEY_CTRL              29
+#define KEY_SPACE             32
+#define KEY_LSHIFT            42
+#define KEY_RSHIFT            54
+#define KEY_ALT               56
+
+#define KEY_F1                128
+#define KEY_F2                129
+#define KEY_F3                130
+#define KEY_F4                131
+#define KEY_F5                132
+#define KEY_F6                133
+#define KEY_F7                134
+#define KEY_F8                135
+#define KEY_F9                136
+#define KEY_F10               137
+
+enum {
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
+};
+
+//-----------------------------------------------
+//------------------  TYPEDEF  ------------------
+//-----------------------------------------------
+//
+typedef unsigned char         uchar;
+typedef unsigned short        ushort;
+typedef unsigned int          uint;
+typedef unsigned long         ulong;
+
+typedef struct { // IRQ HANDLER CALLBACK ARGUENT
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;
+} registers;
+
+// kernel.c
+//
+extern void   kernel_main     (struct multiboot_info * mbi);
+
+// keyboard.c
+//
+extern int		keyboard_getkey	(void);
+extern void   keyboard_wait   (void);
+
+// interrupt.c
+//
+extern void   Intr_Init       (void);
+extern void   Intr_SetHandler (int irq, void (*handler)(registers *r));
+
+// libc.c
+//
+extern uchar  inb             (ushort port);
+extern ushort inw             (ushort port);
+extern void   outb            (ushort port, uchar value);
+//
+extern void   video_clear     (void);
+extern void   video_set_color (unsigned char c);
+extern void		video_backspace	(void);
+extern void   puts            (const char *s);
+
+extern int    strcmp          (const char *s1, const char *s2);
+extern void * memset          (void *s, int c, unsigned int n);
+
+#endif // ! _SIMPLE_H_
+
